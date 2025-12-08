5df6b9770b24ff0616881f77ce9ebdffe0261df1
unknown <povsokny2021@gmail.com>
2025-12-08 08:43:42 -0800

    ss

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+     <h1 className="text-red-600 text-4xl">Weelcome to my channel</h1>
+    </div>
+  );
+}
